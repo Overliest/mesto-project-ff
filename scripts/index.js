c25bc06ec046873b3fe5b198b8c275f9e9c7e5da
Никита <overliest@gmail.com>
2024-11-12 13:17:48 +0300

    Добавил логику добавления и удаления карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,35 @@
-// @todo: Темплейт карточки
+// Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content
 
-// @todo: DOM узлы
+// DOM узлы
+const placesList = document.querySelector('.places__list')
 
-// @todo: Функция создания карточки
+// Функция создания карточки
+const createCard = (data, handleDelete) => {
+  const cardElement = cardTemplate.cloneNode(true).querySelector('.card')
+  
+  const cardImage = cardElement.querySelector('.card__image')
+  const cardTitle = cardElement.querySelector('.card__title')
+  const deleteButton = cardElement.querySelector('.card__delete-button')
 
-// @todo: Функция удаления карточки
+  cardImage.src = data.link
+  cardImage.alt = data.name
+  cardTitle.textContent = data.name
 
-// @todo: Вывести карточки на страницу
+  deleteButton.addEventListener('click', () => {
+      handleDelete(cardElement)
+  });
+
+  return cardElement
+}
+
+// Функция удаления карточки
+const deleteCard = (cardElement) => {
+  cardElement.remove()
+}
+
+// Вывести карточки на страницу
+initialCards.forEach(cardData => {
+  const cardElement = createCard(cardData, deleteCard)
+  placesList.append(cardElement)
+})
